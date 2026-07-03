589cdf0eefa56857ca5706146d67810fce9b0c3e
Shirinmlv <shirin.molavy@study.hs-duesseldorf.de>
2026-07-03 18:42:42 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "DEINE_SUPABASE_URL";
-const SUPABASE_KEY = "DEIN_SUPABASE_ANON_KEY";
-
-export const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
