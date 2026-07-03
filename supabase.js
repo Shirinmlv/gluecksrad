63e005ee3020ae282a2e73ba55d837b6df29d970
Shirinmlv <shirin.molavy@study.hs-duesseldorf.de>
2026-07-03 18:52:46 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "DEINE_SUPABASE_URL";
-const SUPABASE_KEY = "DEIN_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://kuzogsqliihqpjucqqdt.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1em9nc3FsaWlocXBqdWNxcWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwOTMxMjYsImV4cCI6MjA5ODY2OTEyNn0.UdgP451o2iB0nm-OMs1oOO8aUwTZSA51OMEHhzRgB8w";
 
 export const supabase = window.supabase.createClient(
   SUPABASE_URL,
